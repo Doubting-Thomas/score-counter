eebfdd4acff082e19d799530ec25c04ea0b29a58
Doubting-Thomas <thomasmichaelayo@gmail.com>
2022-12-11 11:02:13 -0800

    feat: add bg colors for the results

--- score-counter/script.js
+++ score-counter/script.js
@@ -4,6 +4,7 @@ const scoreAlert = document.querySelector(".alert");
 const btnAdd = document.querySelector(".btn-add");
 const btnReduce = document.querySelector(".btn-reduce");
 const btnReset = document.querySelector(".btn-reset");
+const body = document.querySelector("body");
 
 let score = 1;
 let minScore = 0;
@@ -13,22 +14,26 @@ function addScore() {
   if (score <= maxScore) {
     scoreCount.textContent = score++;
     scoreAlert.textContent = "Continue clicking to reach the final score!";
+    body.style.backgroundColor = "";
   }
 
   if (score > maxScore) {
     score = maxScore;
     scoreAlert.textContent = "You have reached the final score!";
+    body.style.backgroundColor = "#00ff87";
   }
 }
 
 function reduceScore() {
   scoreCount.textContent = score--;
   scoreAlert.textContent = "You are now decreasing the score...";
+  body.style.backgroundColor = "";
 
   if (score < minScore) {
     score = minScore;
     scoreAlert.textContent =
       "You have passed the minimum threshold. Increase the count to continue.";
+    body.style.backgroundColor = "#ff1b6b";
   }
 }
 
@@ -36,6 +41,7 @@ function resetScore() {
   score = "";
   scoreCount.textContent = "";
   scoreAlert.textContent = "";
+  body.style.backgroundColor = "";
 }
 
 btnAdd.addEventListener("click", addScore);
